dc8280345048b9b3cd84a84084fd4be039b2a364
Kyle Schutter <kmosswork@gmail.com>
2018-05-18 22:05:18 -0600

    Initialized then finished the assignment

---
+++ script_8_3.js
@@ -0,0 +1,7 @@
+function printTimesTable(num) {
+    // Loops ten times
+    for (var i = 1; i < 11; i++) {
+        // Prints the line with the output being the given num multiplied by i
+        console.log(num + " * " + i + " = " + num*i);
+    }
+}
